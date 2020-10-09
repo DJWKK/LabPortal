93e0a4cb8ef863d688f3b786025246611c1a7405
Yidaaa-u <68274775+Yidaaa-u@users.noreply.github.com>
2020-10-09 18:26:44 +0800

    陈淼的第一次提交 (#25)
    Co-authored-by: nathaniel-kk <71243650+nathaniel-kk@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -72,3 +72,18 @@ Route::prefix('labhome')->namespace("LabHome")->group(function (){
     Route::get('labarchited','HomeController@labarchited');  //组织架构
     Route::get('labaspect','HomeController@labAspect');  //实验室方向
 });
+/**
+ * @author ChenMiao <github.com//Yidaaa-u>
+ */
+Route::prefix('admin/commmanage')->namespace('Admin\CommManage')->group(function () {
+    Route::get('reviewselect', 'CommController@reviewSelect'); //审查关键字页面敏感词展示
+    Route::post('reviewdel', 'CommController@reviewDel'); //删除审查关键字
+    Route::post('reviewadd', 'CommController@reviewAdd'); //新增审查关键字
+    Route::get('articleselect', 'CommController@articleSelect'); //文章管理页面数据展示
+    Route::post('articledel', 'CommController@articleDel'); //删除文章
+    Route::get('articleselectinfo', 'CommController@articleSelectInfo'); //文章管理页面查看文章的页面展示
+    Route::get('reviewselectinfo', 'CommController@reviewSelectInfo'); //文章和评论管理页面的当前审查关键字展示
+    Route::get('commselect', 'CommController@commSelect'); //评论管理页面评论展示
+    Route::post('commdel', 'CommController@commDel'); //评论管理页面删除评论
+    Route::get('commselectinfo', 'CommController@commSelectInfo'); //评论管理页面查看评论
+});
